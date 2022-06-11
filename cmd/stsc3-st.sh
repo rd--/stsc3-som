2965f487d42a14bd68907f543503ba590766dad3
rohan <rd@slavepianos.org>
2022-06-11 11:43:35 +1000

    renamedirs

--- cmd/stsc3-st.sh
+++ cmd/stsc3-st.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-stsc3-som -cp /home/rohan/sw/stsc3-som/lib/st/:/home/rohan/sw/stsc3-som/lib/testing/ st repl
+dir=/home/rohan/sw/stsc3-som/lib/
+cp=$dir/Smalltalk/:$dir/SUnit/:$dir/Tests/
+stsc3-som -cp $cp st repl
