57e0868a189c55fdbd7a97c2557476ef428a45f8
rohan <rd@slavepianos.org>
2022-06-19 00:21:01 +1000

    ~stsc3

--- cmd/stsc3-st.sh
+++ cmd/stsc3-st.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-libdir=$HOME/sw/stsc3-som/lib/
-oscdir=$HOME/sw/stsc3/som/Osc
-sc3dir=$HOME/sw/stsc3/som/Sc3
-cp=$libdir:$oscdir:$sc3dir
+lib=$HOME/sw/stsc3-som/lib/
+stsc3=$HOME/sw/stsc3/som/
+osc=$stsc3/Osc-Kernel
+sc3=$stsc3/Sc3-Kernel:$stsc3/Sc3-Ugen:$stsc3/Sc3-Pseudo:$stsc3/Sc3-Help
+cp=$lib:$osc:$sc3
 stsc3-som -cp $cp st repl
